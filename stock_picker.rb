6767e50e026210e90adbd6348350531d2a8124eb
JNFussion <juan.jose.gutierrez.99@gmail.com>
2020-09-28 20:21:59 +0200

    formatting

---
+++ stock_picker.rb
@@ -0,0 +1,6 @@
+def stock_picker(array)
+  max = array.combination(2).to_a.max { |a, b| a[1] - a[0] <=> b[1] - b[0] }
+  [array.index(max[0]), array.index(max[1])]
+end
+
+p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
